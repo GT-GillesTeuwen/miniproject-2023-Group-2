62c8e6bb6ed4e512b4ec20ca903913e75920fe6f
RaamnNoodlez99 <ashirbutt998@gmail.com>
2023-04-15 15:34:53 +0200

    fixed error

--- libs/api/profiles/util/src/requests/index.ts
+++ libs/api/profiles/util/src/requests/index.ts
@@ -4,7 +4,6 @@ export * from './update-address-details.request';
 export * from './update-contact-details.request';
 export * from './update-occupation-details.request';
 export * from './update-personal-details.request';
-export * from './update-profile-status.request';
 export * from './get-user-profile.request';
-export * from './'
+export * from './update-profile.request'
 
