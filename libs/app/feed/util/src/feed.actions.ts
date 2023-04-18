c7eca01907e0361ac5287a3db21e10b8c8da1f77
Klairgo <olwagekarel@gmail.com>
2023-04-18 14:22:22 +0200

    Added util to feed page

---
+++ libs/app/feed/util/src/feed.actions.ts
@@ -0,0 +1,21 @@
+import { IProfile } from '@mp/api/profiles/util';
+
+
+export class SetProfile {
+  static readonly type = '[Feed] SetProfile';
+  constructor(public readonly profile: IProfile | null) {}
+}
+
+export class getProfileSuggestions{
+    static readonly type = '[Feed] getProfileSuggestions';
+}
+
+export class updateMatches {
+  static readonly type = '[Feed] updateMatches';
+  constructor(
+    public readonly MatchUserID: string,
+    public readonly ConversationID: string,
+    public readonly MeetingID: string,
+  ) {}
+}
+ 
