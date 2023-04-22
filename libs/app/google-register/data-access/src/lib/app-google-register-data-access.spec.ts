999534b0f2a3d4b9958d4daccd12dafbb4c51e9c
Mekhail Muller <102990592+UnboundGlue@users.noreply.github.com>
2023-04-23 00:17:55 +0200

    Create app-google-register-data-access.spec.ts
    Creation of GoogleRegisterPage
    
    Co-Authored-By: janro13806 <80365036+janro13806@users.noreply.github.com>

---
+++ libs/app/google-register/data-access/src/lib/app-google-register-data-access.spec.ts
@@ -0,0 +1,9 @@
+import { appGoogleRegisterDataAccess } from './app-google-register-data-access';
+
+describe('appGoogleRegisterDataAccess', () => {
+  it('should work', () => {
+    expect(appGoogleRegisterDataAccess()).toEqual(
+      'app-google-register-data-access'
+    );
+  });
+});
