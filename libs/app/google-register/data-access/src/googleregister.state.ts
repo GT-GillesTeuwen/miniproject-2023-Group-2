a0117d07dd4750b926e618ba26325332e36903b2
Mekhail Muller <102990592+UnboundGlue@users.noreply.github.com>
2023-04-23 15:39:32 +0200

    Updated googleregister.state to include email and uid
    removed password section
    
    Co-Authored-By: janro13806 <80365036+janro13806@users.noreply.github.com>

--- libs/app/google-register/data-access/src/googleregister.state.ts
+++ libs/app/google-register/data-access/src/googleregister.state.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Register as AuthRegister } from '@mp/app/auth/util';
+import { GoogleRegister as AuthGoogleRegister } from '@mp/app/auth/util';
 import { SetError } from '@mp/app/errors/util';
-import { Register } from '@mp/app/register/util';
+import { GoogleRegister } from '@mp/app/google-register/util';
 import { Action, State, StateContext } from '@ngxs/store';
 
 export interface GoogleRegisterStateModel {
   registerForm: {
     model: {
+      uid: string | null;
       firstName: string | null;
       lastName: string | null;
       gender: string | null;
@@ -25,6 +26,7 @@ export interface GoogleRegisterStateModel {
   defaults: {
     registerForm: {
       model: {
+        uid: null,
         firstName: null,
         lastName: null,
         gender: null,
@@ -40,19 +42,21 @@ export interface GoogleRegisterStateModel {
 })
 @Injectable()
 export class GoogleRegisterState {
-  @Action(Register)
+  @Action(GoogleRegister)
   async register(ctx: StateContext<GoogleRegisterStateModel>) {
     try {
+      //alert("Reached state.ts")
       const state = ctx.getState();
+      const uid = state.registerForm.model.uid;
       const email = state.registerForm.model.email;
       const firstName = state.registerForm.model.firstName;
       const lastName = state.registerForm.model.lastName;
       const gender = state.registerForm.model.gender;
       const age = state.registerForm.model.age;
-      const password = state.registerForm.model.password;
-
-      if (email && firstName && lastName && gender && age && password) {
-        return ctx.dispatch(new AuthRegister(gender ,age, firstName, lastName ,email, password));
+      //const password = state.registerForm.model.password;
+      //alert(email! + firstName + lastName)
+      if (email && firstName && lastName && gender && age) {
+        return ctx.dispatch(new AuthGoogleRegister(uid!, gender ,age, firstName, lastName ,email));
       }
       return ctx.dispatch(new SetError('Invalid input'));
     } catch (error) {
