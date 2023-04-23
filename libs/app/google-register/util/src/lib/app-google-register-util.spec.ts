add7ab4a3f22266999a793f0717d53ac632f1516
Mekhail Muller <102990592+UnboundGlue@users.noreply.github.com>
2023-04-23 02:23:24 +0200

    Create app-google-register-util.spec.ts
    Co-Authored-By: janro13806 <80365036+janro13806@users.noreply.github.com>

---
+++ libs/app/google-register/util/src/lib/app-google-register-util.spec.ts
@@ -0,0 +1,7 @@
+import { appGoogleRegisterUtil } from './app-google-register-util';
+
+describe('appGoogleRegisterUtil', () => {
+  it('should work', () => {
+    expect(appGoogleRegisterUtil()).toEqual('app-google-register-util');
+  });
+});
