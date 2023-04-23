86ba31d4fe35dd47ba646001fcee4e2a9bfd97b5
Mekhail Muller <102990592+UnboundGlue@users.noreply.github.com>
2023-04-23 15:43:15 +0200

    Implemented actionsExecuting obserable
    Still broken :(
    
    Co-Authored-By: janro13806 <80365036+janro13806@users.noreply.github.com>

--- libs/app/welcome/feature/src/welcome.page.ts
+++ libs/app/welcome/feature/src/welcome.page.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { ContinueWithGoogle } from '@mp/app/welcome/util';
-import { Store } from '@ngxs/store';
+import {
+  ActionsExecuting,
+  actionsExecuting
+} from '@ngxs-labs/actions-executing';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ms-welcome-page',
@@ -8,6 +13,9 @@ import { Store } from '@ngxs/store';
   styleUrls: ['./welcome.page.scss'],
 })
 export class WelcomePage {
+  @Select(actionsExecuting([ContinueWithGoogle])) 
+  busy$!: Observable<ActionsExecuting>;
+
   constructor(private readonly store: Store) {}
 
   continueWithGoogle() {
