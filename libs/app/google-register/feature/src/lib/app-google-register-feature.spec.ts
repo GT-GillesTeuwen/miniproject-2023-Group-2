a9c375344beb795902cab25a53e929cf3969e07e
Mekhail Muller <102990592+UnboundGlue@users.noreply.github.com>
2023-04-23 02:23:01 +0200

    Create app-google-register-feature.spec.ts
    Co-Authored-By: janro13806 <80365036+janro13806@users.noreply.github.com>

---
+++ libs/app/google-register/feature/src/lib/app-google-register-feature.spec.ts
@@ -0,0 +1,7 @@
+import { appGoogleRegisterFeature } from './app-google-register-feature';
+
+describe('appGoogleRegisterFeature', () => {
+  it('should work', () => {
+    expect(appGoogleRegisterFeature()).toEqual('app-google-register-feature');
+  });
+});
