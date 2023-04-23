519447e52da907d158c61cd815235a6b16468510
Mekhail Muller <102990592+UnboundGlue@users.noreply.github.com>
2023-04-23 02:22:56 +0200

    Create googleregister.routing.ts
    Co-Authored-By: janro13806 <80365036+janro13806@users.noreply.github.com>

---
+++ libs/app/google-register/feature/src/googleregister.routing.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GoogleRegisterPage } from './googleregister.page';
+
+import { VerifyPageComponent } from 'libs/app/chat/feature/src/lib/verify-page/verify-page.component';
+import { MessagesPageComponent } from 'libs/app/chat/feature/src/lib/messages-page/messages-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: GoogleRegisterPage,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class RegisterRouting {}
