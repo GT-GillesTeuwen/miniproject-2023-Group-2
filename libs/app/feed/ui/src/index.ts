fc590d910b00c9b66c12d5081e0c9a66e7622cf9
kphutiyagae <kopanophutiyagae@gmail.com>
2023-04-23 11:50:08 +0200

    feat(none): :construction: WIP: Added UI library to feed library.

---
+++ libs/app/feed/ui/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/app-feed-ui.module';
+
+export * from './lib/lib.routes';
---
+++ libs/app/feed/ui/src/lib/lib.routes.ts
@@ -0,0 +1,5 @@
+import { Route } from '@angular/router';
+
+export const appFeedUiRoutes: Route[] = [
+  /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+];
