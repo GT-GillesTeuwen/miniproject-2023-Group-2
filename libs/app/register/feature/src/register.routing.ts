b0c91443006ab574366b741d1d66939851abda00
Mekhail Muller <102990592+UnboundGlue@users.noreply.github.com>
2023-04-23 02:24:34 +0200

    Included routing within Register
    Co-Authored-By: janro13806 <80365036+janro13806@users.noreply.github.com>

--- libs/app/register/feature/src/register.routing.ts
+++ libs/app/register/feature/src/register.routing.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterPage } from './register.page';
 
+import { VerifyPageComponent } from 'libs/app/chat/feature/src/lib/verify-page/verify-page.component';
+import { MessagesPageComponent } from 'libs/app/chat/feature/src/lib/messages-page/messages-page.component';
+
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
+    component: RegisterPage,
+  },
+  {
+    path: 'register/complete',
+    pathMatch : 'full',
     component: RegisterPage,
   },
 ];
