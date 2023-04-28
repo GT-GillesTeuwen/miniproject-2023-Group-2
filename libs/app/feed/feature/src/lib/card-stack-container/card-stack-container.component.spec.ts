51323096e3bcc64672e53a6fbe85acccbed0450b
Klairgo <olwagekarel@gmail.com>
2023-04-28 22:21:20 +0200

    Finished testing for match user function in feed

--- libs/app/feed/feature/src/lib/card-stack-container/card-stack-container.component.spec.ts
+++ libs/app/feed/feature/src/lib/card-stack-container/card-stack-container.component.spec.ts
@@ -53,4 +53,64 @@ describe('CardStackContainerComponent', () => {
     expect(StoreMock.dispatch).toHaveBeenCalledTimes(4);
 
   });
+
+  it('should match user where the other user did match', () => {
+
+    component.currentUserID = '123'
+    const match: IMatchDetails = {
+      MatchUserID: '123',
+      PairID: '12343',
+      MatchStatus: 'SENT'
+    }
+
+    component.profilesToShow[0] = {
+      UID: '123',
+      Matches: [match]
+    }
+
+    component.matchUsers(true);
+
+    expect(StoreMock.dispatch).toHaveBeenCalledTimes(4);
+
+  });
+
+  it('should remove user that the curr user swipe left on and the other user did not like me', () => {
+
+    component.currentUserID = '123'
+    const match: IMatchDetails = {
+      MatchUserID: '12',
+      PairID: '12343',
+      MatchStatus: 'SENT'
+    }
+
+    component.profilesToShow[0] = {
+      UID: '123',
+      Matches: [match]
+    }
+
+    component.matchUsers(false);
+
+    expect(StoreMock.dispatch).toHaveBeenCalledTimes(3);
+
+  });
+
+  it('should remove user that the curr user swipe left on and the other user did like me', () => {
+
+    component.currentUserID = '123'
+    const match: IMatchDetails = {
+      MatchUserID: '123',
+      PairID: '12343',
+      MatchStatus: 'SENT'
+    }
+
+    component.profilesToShow[0] = {
+      UID: '123',
+      Matches: [match]
+    }
+
+    component.matchUsers(false);
+
+    expect(StoreMock.dispatch).toHaveBeenCalledTimes(4);
+
+  });
 });
