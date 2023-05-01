a91e2fee431dde5f7f83d66cd4e5891fd9455368
kphutiyagae <kopanophutiyagae@gmail.com>
2023-05-01 02:26:18 +0200

    "Added e2e Cypress tests for login"

--- cypress/e2e/testSpec.cy.ts
+++ cypress/e2e/testSpec.cy.ts
@@ -1,6 +1,54 @@
-describe('template spec', () => {
-  it('passes', () => {
+
+describe('tests landing page', () => {
+  beforeEach(() => {
     cy.visit('/')
     cy.on('uncaught:exception', (err, runnable) => { return false; })
   })
+
+  it('should have routing buttons defined', () => {  
+    cy.get('.button').contains('Login')
+    cy.get('.button').contains('Register')
+  })
+
+  it('should navigate to login', () => {  
+    cy.get('.button').contains('Login').click()
+    cy.url().should('include', '/login')
+  })
+
+  it('should navigate to signup', () => {  
+    cy.get('.button').contains('Login').click()
+    cy.url().should('include', '/login')
+  })
+
+  it('should navigate to login', () => {  
+    cy.get('.button').contains('Login').click()
+    cy.url().should('include', '/login')
+  })
+
+  it('should attempt to login with only email', () => {  
+    cy.get('.button').contains('Login').click()
+    cy.url().should('include', '/login')
+
+    cy.fixture('test.credentials.json')
+    .then( userCredentials => {
+      cy.get('input').first().type(userCredentials[0].email)
+      cy.get('.button').should('be.disabled')
+    })
+
+  })
+
+  it('should attempt to login with incorrect credentials', () => {  
+    cy.get('.button').contains('Login').click()
+    cy.url().should('include', '/login')
+
+    cy.fixture('test.credentials.json')
+    .then( userCredentials => {
+      cy.get("#emailInput").type(userCredentials[0].email)
+      cy.get("#passwordInput").type(userCredentials[0].password)
+      cy.get('#loginButton').click()
+      cy.url().should('include', '/login')
+    })
+
+  })
+
 })
